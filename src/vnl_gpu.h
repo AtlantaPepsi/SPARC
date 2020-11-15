4a111dd6f27447b6c2ae9971976d0fa44185fe96
Tim <43156029+AtlantaPepsi@users.noreply.github.com>
2020-11-15 16:01:27 -0500

    Update vnl_gpu.h

--- src/vnl_gpu.h
+++ src/vnl_gpu.h
@@ -14,7 +14,20 @@ void Vnl_gpu(const min_SPARC_OBJ *pSPARC, const ATOM_NLOC_INFLUENCE_OBJ *Atom_In
              const NLOC_PROJ_OBJ *nlocProj,
              const min_SPARC_OBJ *d_SPARC, const ATOM_NLOC_INFLUENCE_OBJ *d_Atom_Influence_nloc,
              const NLOC_PROJ_OBJ *d_locProj,
-             const int DMnd, const int ncol, double *d_x, double *d_Hx, int GPUDirect);
+             const int DMnd, const int ncol, double *d_x, double *d_Hx, MPI_Comm comm, int GPUDirect);
+
+  __global__
+void x_rc(double *d_xrc, double *d_x, const ATOM_NLOC_INFLUENCE_OBJ *d_Atom_Influence_nloc,
+          int ncol, int type, int atom, int DMnd);
+  
+  __global__
+void Vnl_gammaV(const min_SPARC_OBJ *d_SPARC, double *d_alpha, int ncol);
+  
+  __global__
+void update(double *d_Hx, double *Vnlx, const ATOM_NLOC_INFLUENCE_OBJ *d_Atom_Influence_nloc,
+            int ncol, int type, int atom, int DMnd);
+  
+
 
 
 
